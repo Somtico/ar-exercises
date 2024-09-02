92dd1ab6415adf30214b4b9e567a0d279231ca41
Somto Ufondu <somtoufondu@gmail.com>
2024-09-02 14:53:03 -0600

    Add calculations for total revenue, average revenue, and stores with M+ revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for all stores: #{total_revenue}"
+
+# Output average annual revenue for all stores
+average_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue for all stores: #{average_revenue}"
+
+# Output the number of stores generating $1M or more in annual sales
+stores_over_1m = Store.where('annual_revenue >= ?', 1_000_000).count
+puts "Number of stores generating $1M or more in annual sales: #{stores_over_1m}"
