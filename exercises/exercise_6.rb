b192e3221d5347fa6a185ba8f8126f0252943d25
Somto Ufondu <somtoufondu@gmail.com>
2024-09-02 14:56:55 -0600

    Add one-to-many association between Store and Employee; create sample employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# Create some employees for @store1 (Burnaby)
+@store1 = Store.find(1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 55)
+
+# Create some employees for @store2 (Richmond)
+@store2 = Store.find(2)
+@store2.employees.create(first_name: "Alice", last_name: "Johnson", hourly_rate: 70)
+@store2.employees.create(first_name: "Bob", last_name: "Brown", hourly_rate: 65)
